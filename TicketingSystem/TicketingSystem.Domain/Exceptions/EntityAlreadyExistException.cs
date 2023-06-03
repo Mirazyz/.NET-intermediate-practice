b8c2a75a8f418608096c6ad1844fb351a365ae37
Miraziz.Khidoyatov <miraziz_khidoyatov@epam.com>
2023-06-03 17:24:39 +0500

    Add custom exceptions

---
+++ TicketingSystem/TicketingSystem.Domain/Exceptions/EntityAlreadyExistException.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace TicketingSystem.Domain.Exceptions;
+
+public class EntityAlreadyExistException : Exception
+{
+    public EntityAlreadyExistException()
+        : base()
+    {
+    }
+
+    public EntityAlreadyExistException(string message)
+        : base(message)
+    {
+    }
+
+    public EntityAlreadyExistException(string message, params object[] args)
+        : base(string.Format(CultureInfo.CurrentCulture, message, args))
+    {
+    }
+}
---
+++ TicketingSystem/TicketingSystem.Domain/Exceptions/EntityDoesNotExistException.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace TicketingSystem.Domain.Exceptions;
+
+public class EntityDoesNotExistException : Exception
+{
+    public EntityDoesNotExistException()
+        : base()
+    {
+    }
+
+    public EntityDoesNotExistException(string message)
+        : base(message)
+    {
+    }
+
+    public EntityDoesNotExistException(string message, params object[] args)
+        : base(string.Format(CultureInfo.CurrentCulture, message, args))
+    {
+    }
+}
